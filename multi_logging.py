9c1420ceb8cfed1ee4674d1d9621008d6cda2549
Frank <vfa1357503@fed.cclrc.ac.uk>
2018-04-23 12:35:06 +0100

    Added required logging file

---
+++ multi_logging.py
@@ -0,0 +1,9 @@
+import logging
+
+def logger_thread(q):
+    while True:
+        record = q.get()
+        if record is None:
+            break
+        logger = logging.getLogger(record.name)
+        logger.handle(record)
